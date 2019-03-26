ae8f52ec9b8e0e8f0146f6e28c21191fab252b64
hobetuz <hobetuz@ipartekaula.com>
2019-03-26 19:51:52 +0100

    He creado mi BBDD provisional

---
+++ src/app/mock-producto.ts
@@ -0,0 +1,9 @@
+import { Producto } from './producto';
+
+export const PRODUCTOS: Producto[]=[
+    {id: 1, nombre: "Pera",descripcion:"El rincon de Soto",precio:2.35},
+    {id: 2, nombre: "Kiwi",descripcion:"Zespri",precio:3.15},
+    {id: 3, nombre: "Manzana",descripcion:"De tipo Fuji",precio:0.99},
+    {id: 4, nombre: "Platanos",descripcion:"Canarias",precio:1.99}
+]
+
