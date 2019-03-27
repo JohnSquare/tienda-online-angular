cb67d5797d3fc253530bd81fdfe6c4140c851fc1
hobetuz <hobetuz@ipartekaula.com>
2019-03-27 20:03:33 +0100

    Muestro el componente oculto con routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TablaProductosComponent } from './tabla-productos/tabla-productos.component';
 
 const routes: Routes = [
-  
+  { path: 'tabla-productos', component: TablaProductosComponent }  
 ];
 
 @NgModule({
